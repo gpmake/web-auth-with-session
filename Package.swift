a853f37b9555c00d94bd03aa0230c8ebf3405e73
Martin J. Lasek <martinlasek@hotmail.de>
2018-09-09 20:31:05 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,10 @@ import PackageDescription
 let package = Package(
     name: "web-auth-with-session",
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
-	    .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+	.package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
     ],
     targets: [
