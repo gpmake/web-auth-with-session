0fa8ea5485a4fd0b708f2d3720c0fc128bd65122
Martin J. Lasek <martinlasek@hotmail.de>
2018-09-09 22:23:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
     ],
     targets: [
